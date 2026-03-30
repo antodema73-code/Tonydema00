92d5f48b80d842896b50c017bbf8e2dad28adeb9
Antonio Demartino <Demartino@pontillo.local>
2026-03-30 10:15:02 +0200

    Increased next lap preview font size for mobile

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'workout-cache-v5';
+const CACHE_NAME = 'workout-cache-v6';
 const ASSETS_TO_CACHE = [
     './', './index.html', './styles.css',
     './app.js', './audio.js', './wakeLock.js', './manifest.json'
